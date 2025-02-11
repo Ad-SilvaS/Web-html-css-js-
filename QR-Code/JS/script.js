74c6e53ff368c175b693b01d70421f64bf889fdd
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-02-11 15:13:47 -0300

    Add files via upload

---
+++ QR-Code/JS/script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('.container');
+const qrCodeInput = document.querySelector('#qr-form input');
+const qrCodeBtn = document.querySelector('#qr-form button');
+const qrCodeIMG = document.querySelector('#qr-code img');
+
+function generateQrCode(){
+    const qrCodeInputValues = qrCodeInput.value;
+    if(!qrCodeInputValues) return;
+
+    qrCodeBtn.innerText = 'Gerando Código...';
+    qrCodeIMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example`;
+    qrCodeIMG.addEventListener('load', ()=>{
+        container.classList.add('active');
+        qrCodeBtn.innerText = 'Código Gerado!';
+    });
+};
+
+qrCodeBtn.addEventListener('click', ()=>{
+    generateQrCode();
+});
+
+qrCodeInput.addEventListener('keydown', (e)=>{
+    if(e.code == 'Enter'){
+        generateQrCode();
+    };
+});
+
+qrCodeInput.addEventListener('keyup', ()=>{
+    if(!qrCodeInput.value){
+        container.classList.remove('active');
+        qrCodeBtn.innerText = 'Gerar QR CODE';
+    };
+});
