d88e7c8f4f689f8b818ded3e3ba46b2f7ac76296
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-03-19 00:38:24 -0300

    Update script.js

--- SignIn-SignUp/JS/script.js
+++ SignIn-SignUp/JS/script.js
@@ -11,3 +11,20 @@ toRegister.addEventListener('click', ()=>{
     card.classList.remove('login-active');
     card.classList.add('register-active');
 });
+
+const tl = gsap.timeline({defaults: {
+    duration: 2.5
+}});
+
+tl.from('.card', {
+    opacity: 0,
+    x: -2000
+}),
+tl.from('.left-card', {
+    opacity: 0,
+    y: -2000
+}, '-= .2'),
+tl.from('.right-card', {
+    opacity: 0,
+    y: -2000
+}, '-= .2')
